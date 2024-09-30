6ad9403ce86ac59b70bb3eafd44a466b9e86bbbc
Janice Thomas <30435104+janicethomas@users.noreply.github.com>
2024-10-01 02:10:57 +0530

    Add files via upload

--- face_rec_img.py
+++ face_rec_img.py
@@ -1,26 +1,44 @@
 import cv2
 import numpy as np
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+import firebase_admin
+from firebase_admin import credentials, storage
 
-image_path = 'janice.jpg'
-image = cv2.imread(image_path)
+cred = credentials.Certificate('./anti-theft-detection-2d6d8-firebase-adminsdk.json')
+app = firebase_admin.initialize_app(cred, {'storageBucket': 'anti-theft-detection-2d6d8.appspot.com'})
+bucket = storage.bucket()
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def get_img():
+    blob = bucket.get_blob("unknown.jpg")
+    if blob:
+        img_data = blob.download_as_bytes()
+        arr = np.frombuffer(img_data, np.uint8)
+        img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+        return img
+    else:
+        print("Blob not found!")
+        exit()
 
-faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read('trainer/trainer.yml')
+def face_recognition(img):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    face_region = gray[y:y+h, x:x+w]
-    label, confidence = recognizer.predict(face_region)
-    names = {1: 'Janice', 0: 'Unknown'}
-    name = names.get(label, 'Unknown')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    cv2.putText(image, f'{name} ({int(confidence)})', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    recognizer.read('trainer/trainer.yml')
 
-cv2.imshow('Face Recognition', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        face_region = gray[y:y+h, x:x+w]
+        label, confidence = recognizer.predict(face_region)
+        names = {1: 'Janice', 0: 'Unknown'}
+        name = names.get(label, 'Unknown')
+
+        cv2.putText(img, f'{name} ({int(confidence)})', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+
+    cv2.imshow('Face Recognition', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+face_recognition(get_img())
